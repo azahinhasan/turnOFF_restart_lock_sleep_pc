a9bfaa72090ea5c16a29c216d6109dff7fef1729
A H Zahin <azahinhasan@gmail.com>
2020-12-14 11:56:04 +0600

    1.1.0

--- trunOFFpc.py
+++ trunOFFpc.py
@@ -6,8 +6,8 @@ import sys
 from ctypes import *
 from time import sleep
 
-def lockActin():
-    windll.user32.BlockInput(False)
+#def lockActin():
+    #windll.user32.BlockInput(False)
 
 def loopFor(value):
     for i in range(value):
@@ -17,11 +17,8 @@ def loopFor(value):
 def actionAfter(waitTime,actionName):
 
     totalTime = waitTime-1
+    loopFor(2)
     print("Time Left: ")
-    #for i in range(waitTime):      
-    #    time.sleep(1)
-     #   print(totalTime)
-     #   totalTime-=1
 
     sys.stdout.flush()
     some_list = [0] * waitTime
@@ -32,55 +29,93 @@ def actionAfter(waitTime,actionName):
         sys.stdout.write(msg + chr(8) * len(msg))
         sys.stdout.flush()
         sleep(1)
+    print()
 
     
 
-    
+def actionName():
+    options = input("Chose your option: 1> ShutDown ::: 2> Restart ::: 3> Logout:-> ")
+    if options == '1':
+        return '1'
+    elif options == '2':
+        return '2'
+    elif options == '3':
+        return '3' 
+    else:
+        loopFor(2)
+        print("Wrong Input!!!!!")
+        print("Try again....")
+        actionName()
 
-waitTime=0
 
-options = input("Chose your option: 1> ShutDown ::: 2> Restart ::: 3> Logout:-> ")
+def actionTimeType():
+    options = input("Enter the Time Type: 1>Hour ::: 2>Minutes ::: 3>Secound :-> ")
+    if options == '1':
+        return actonTimeDuration('1') 
+    elif options == '2':
+        return actonTimeDuration('2') 
+    elif options == '3':
+        return actonTimeDuration('3') 
+    else:
+        loopFor(2)
+        print("Wrong Input!!!!!")
+        print("Try again....")
+        actionTimeType()
+
+
+def actonTimeDuration(options2):
+    loopFor(3)
+    waitTime=0
+    if options2 == '1':
+        waitTime= input("Hours :-> ")
+        waitTime=int(waitTime)*60*60
+    elif options2 == '2':
+        waitTime= input("Minutes :-> ")
+        waitTime=int(waitTime)*60
+    elif options2 == '3':
+        waitTime= input("Secound :-> ")
+        waitTime=int(waitTime)
+
+    return waitTime
+
+
+
+def finalAction(options,waitTime):
+    if options == '1':
+        print("Running..........")
+        actionAfter(waitTime,'PC Shutdown')
+        os.system("shutdown /s /t 1")
+
+    elif options == '2':
+        print("Running..........")
+        actionAfter(waitTime,'PC Restart')
+        os.system("shutdown /r /t 1")
+    elif options == '3':
+        print("Running..........")
+        actionAfter(waitTime,'Sceen Lock')
+        print("done")
+        #ctypes.windll.user32.LockWorkStation()
+
+
+
+
+
+
+
+#main
+
+options = actionName()
 loopFor(3)
-options2 = input("Enter the Time Type: 1>Hour ::: 2>Minutes ::: 3>Secound :-> ")
+waitTime = actionTimeType()
 loopFor(3)
-if options2 == '1':
-    waitTime= input("Hours :-> ")
-    waitTime=int(waitTime)*60*60
-elif options2 == '2':
-    waitTime= input("Minutes :-> ")
-    waitTime=int(waitTime)*60
-elif options2 == '3':
-    waitTime= input("Secound :-> ")
-    waitTime=int(waitTime)
+
+options3 = input("Are you sure about the action? : 1> Yes ::: 2> No  :-> ")
+
+loopFor(3)
+if options3 == '1':
+    finalAction(options,waitTime)
 else:
-    print("Try Again....")
     exit()
-
-
-loopFor(3)
-options3 = input("Are you sure about the action: 1> Yes ::: 2> No  :-> ")
-
-loopFor(3)
-
-
-if options == '1' and options3 == '1':
-    print("Running..........")
-    actionAfter(waitTime,'Shutdown')
-    os.system("shutdown /s /t 1")
-
-elif options == '2' and options3 == '1':
-    print("Running..........")
-    actionAfter(waitTime,'Restart')
-    os.system("shutdown /r /t 1")
-elif options == '3' and options3 == '1':
-    print("Running..........")
-    actionAfter(waitTime,'Sceen Lock')
-    #print("done")
-    ctypes.windll.user32.LockWorkStation()
-
-else:
-    exit()
-
 
 
 
