ccd26c8aceef7490dc4354825276af2e42c48380
A H Zahin <azahinhasan@gmail.com>
2020-12-14 11:01:08 +0600

    11.01am

--- trunOFFpc.py
+++ trunOFFpc.py
@@ -2,34 +2,46 @@ from tkinter import *
 import os
 import time
 import ctypes 
+import sys
 from ctypes import *
-
+from time import sleep
 
 def lockActin():
     windll.user32.BlockInput(False)
 
-def loopFor():
-    for i in range(3):
+def loopFor(value):
+    for i in range(value):
         print("---")
 
 
-def actionAfter(waitTime):
+def actionAfter(waitTime,actionName):
 
     totalTime = waitTime
     print("Time Left: ")
-    for i in range(waitTime):      
-        time.sleep(1)
-        print(totalTime)
-        totalTime-=1
+    #for i in range(waitTime):      
+    #    time.sleep(1)
+     #   print(totalTime)
+     #   totalTime-=1
+
+    sys.stdout.flush()
+    some_list = [0] * waitTime
+    loopFor(5)
+    for idx, item in enumerate(some_list):
+        msg = "%s in %i Sec....." % (actionName,idx)
+        sys.stdout.write(msg + chr(8) * len(msg))
+        sys.stdout.flush()
+        sleep(1)
+
+    
 
     
 
 waitTime=0
 
 options = input("Chose your option: 1> ShutDown ::: 2> Restart ::: 3> Logout:-> ")
-loopFor()
+loopFor(3)
 options2 = input("Enter the Time Type: 1>Hour ::: 2>Minutes ::: 3>Secound :-> ")
-loopFor()
+loopFor(3)
 if options2 == '1':
     waitTime= input("Hours :-> ")
     waitTime=int(waitTime)*60*60
@@ -44,26 +56,26 @@ else:
     exit()
 
 
-loopFor()
+loopFor(3)
 options3 = input("Are you sure about the action: 1> Yes ::: 2> No  :-> ")
 
-loopFor()
+loopFor(3)
 
 
 if options == '1' and options3 == '1':
     print("Running..........")
-    actionAfter(waitTime)
-    os.system("shutdown /s /t 1")
+    actionAfter(waitTime,'Shutdown')
+    #os.system("shutdown /s /t 1")
 
 elif options == '2' and options3 == '1':
     print("Running..........")
-    actionAfter(waitTime)
-    os.system("shutdown /r /t 1")
+    actionAfter(waitTime,'Restart')
+    #os.system("shutdown /r /t 1")
 elif options == '3' and options3 == '1':
     print("Running..........")
-    actionAfter(waitTime)
-    #print("done")
-    ctypes.windll.user32.LockWorkStation()
+    actionAfter(waitTime,'Sceen Lock')
+    print("done")
+    #ctypes.windll.user32.LockWorkStation()
 
 else:
     exit()
